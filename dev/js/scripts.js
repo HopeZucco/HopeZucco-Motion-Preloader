71d01a9c0c5921f5711f617fb40c66b707c5ee39
HopeZucco <hmz34@drexel.edu>
2022-05-17 17:51:51 -0400

    initial commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,26 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/MorphSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+
+//Gsap Register Plugins
+
+gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin (MorphSVGPlugin);
+gsap.registerPlugin (DrawSVGPlugin);
+gsap.registerPlugin (MotionPathHelper);
+
+const mainTL = gsap.timeline();
+
+maintTL.from("#swirl", {duration: 3, drawSVG:0})
+
+
+
+;
+
+
+
+
+
+GSDevTools.create();
