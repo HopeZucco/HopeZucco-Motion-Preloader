922b5c8dc6ebcdc49fbb4d55d3bc301142209a59
HopeZucco <hmz34@drexel.edu>
2022-05-19 17:28:35 -0400

    opacities

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,14 +14,22 @@ const mainTL = gsap.timeline();
  
  function setStage(){
     let tl = gsap.timeline();
+    
+tl
+.fromTo(".swirl", {drawSVG:"0%"}, {fillOpacity:0, duration: 1, drawSVG:"100%"}, "+=0.1")
+.fromTo(".side", {drawSVG:"0%", duration: 0}, {fillOpacity:0, duration: 2, drawSVG:"100%"}, "+=0.1")
+.fromTo(".handle", {drawSVG:"0%", duration: 0}, {fillOpacity:0, duration: 2, drawSVG:"100%"}, "+=0.1")
+.Fromto(".swirl", 3, {fillOpacity: 1, strokeOpacity: 0})
+.Fromto(".side", 3, {fillOpacity: 1, strokeOpacity: 0})
+.to(".handle", 3, {fillOpacity: 1, strokeOpacity: 0})
 
-    tl
-    .set(".swirl", {x:"-=175", alpha:0})
-    .set(".side", {x:"-=175", alpha:0})
-    .set(".handle", {x:"-=175", alpha:0})
+    // tl
+    // .set(".swirl", {x:"-=175", alpha:0})
+    // .set(".side", {x:"-=175", alpha:0})
+    // .set(".handle", {x:"-=175", alpha:0})
 
 
-     ;
+    //  ;ko 
 
     return tl;
 
@@ -32,9 +40,7 @@ function firstAnimation(){
     
         tl
         //.from (".swirl", {duration:.75, xPercent:-50, rotation:360, alpha:0})
-        .to(".swirl", {duration:1.5, x:0, alpha:1})
-        .to(".side", {duration:1, x:0, alpha:1})
-        .to(".handle", {duration:1, x:0, alpha:1})
+        // .to(".swirl", {duration:3, x:0, alpha:1})
 
         //.to(".swirl", {duration: 3, drawSVG:"0% 100%"})
 
